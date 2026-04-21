f030209b72ba835b2b20022c65b7d14640641b24
Alpha Malick DIAWARA <102668503+x225franc@users.noreply.github.com>
2026-04-21 23:30:40 +0200

    icon added

--- src/main.js
+++ src/main.js
@@ -27,6 +27,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, '../../assets/icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
